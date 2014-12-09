e988c283dfed9cc1613563001b3599d9fe78e2e3
cesarpachon <cesarpachon@gmail.com>
2014-12-09 17:33:36 -0500

    adding crossword

--- www/js/drag.js
+++ www/js/drag.js
@@ -2,9 +2,8 @@ var Drag = (function($){
   "use strict";
   var drag = {};
 
-  //array of booleans
-  var _answers = [];
-  var _validanswers = [];
+  var _dropped_total = 0;
+  var _dropped_counter = 0;
 
 	var _svg_container = null;
 
@@ -32,12 +31,9 @@ var Drag = (function($){
 	*/
 	drag.add_droppable = function(svg_item_id){
 		var svg_item = $("#"+svg_item_id)[0];
-		//var matrix = svg_item.getScreenCTM();
-
 
 		//get the height of the document
 		var docheight = parseInt($("svg").attr("height"));
-		console.log("docheight: ", docheight);
 
 		//get the global offset of inkscape
 		var transformstr = $("#layer1").attr("transform");
@@ -47,9 +43,6 @@ var Drag = (function($){
 		var offx = parseInt(tokens[0]);
 		var offy = parseInt(tokens[1]);
 
-		console.log("offx: ", offx, "offy:", offy);
-
-
 		function getAttribPx(item, attr){
 			var val = item.getAttribute(attr);
 			return Math.floor(val);
@@ -58,57 +51,37 @@ var Drag = (function($){
 		var x = getAttribPx(svg_item, "x");
 		var y = getAttribPx(svg_item, "y");
 
-		console.log("svg_ittem attribs x, y: ", x, y);
-
-
 		var left = (x + offx) + "px";
 				var top =  (y- docheight /*- offy */) + "px";
 
-		var div = "<div class='drop_item' style=' ";
+		var div = "<div id='"+svg_item_id+"' class='drop_item' style=' ";
 		div += "left: "+ left + "; ";
 		div += "top: "+ top+ "; ";
 		div += "'>"+svg_item_id+"</div>";
-		console.log(div);
-		_svg_container.append($(div));
+
+    var $div = $(div);
+    _svg_container.append($div);
+
+    $div.droppable();
+
+
+    $div.droppable({
+      drop: function( event, ui ) {
+        if(this.id === ui.draggable.attr("id")){
+          _dropped_counter++;
+        }
+      }
+    });
+
+    _dropped_total++;
 	};
 
 
-  drag.add_question = function(question, validanswer){
-    console.log("adding question");
-    var id = "question_"+_answers.length;
-    var $input = $(
-       "<li>"+
-      '<div id="'+id+'">'
-      +'<p class="question">'
-      + question+'</p>'
-      +'<input type="radio" id="'+id+'1" name="'+id+'"><label for="'+id+'1">True</label>'
-      +'<input type="radio" id="'+id+'2" name="'+id+'" checked="checked"><label for="'+id+'2">False</label>'
-      +'</div>'
-      +"</li>");
-
-    $input.buttonset();
-
-    $input.on("click", "*[type='radio']", function(){
-      var $this = $(this);
-      var _q = $this.attr('name');
-      var _val = $this.attr('id').substr(_q.length) === "1";
-      var _index = parseInt(_q.substr("question_".length));
-      _answers[_index] = _val;
-    });
-
-    $('ol.falsetrue_questions').append($input);
-
-    _answers.push(false);
-    _validanswers.push(validanswer);
-  };
 
 
   drag.validate = function(){
 
-    var valid = true;
-    for(var i = 0; i<_answers.length; ++i){
-      valid = valid && (_answers[i] === _validanswers[i]);
-    }
+    var valid = _dropped_total === _dropped_counter;
 
     if(valid){
       $('.feedback_bad').hide(100);
@@ -121,12 +94,14 @@ var Drag = (function($){
   };
 
   drag.reset = function(){
-      $('.feedback_ok').hide(100);
-      $('.feedback_bad').hide(100);
-    console.log("reset");
-    for(var i=0; i< _answers.length; ++i){
-      _answers[i] = false;
-    }
+    $('.feedback_ok').hide(100);
+    $('.feedback_bad').hide(100);
+    _dropped_counter = 0;
+
+    //in order to reset the draggables, lets remove the left and top attributes
+     $( ".drag_item" ).css("left", "");
+     $( ".drag_item" ).css("top", "");
+
   };
 
   return drag;
